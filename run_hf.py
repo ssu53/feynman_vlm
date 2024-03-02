a04f3096472ceee261a0a3853f2075ab450ef85c
Shiye Su <shiyesu@rnn.ist.berkeley.edu>
2024-03-02 04:49:29 -0800

    argparse, bitsandbytes quantisation

--- run_hf.py
+++ run_hf.py
@@ -5,58 +5,20 @@ import pandas as pd
 from PIL import Image
 from tqdm.auto import tqdm
 from transformers import AutoProcessor, LlavaForConditionalGeneration, AutoTokenizer
+import argparse
+from tqdm import tqdm
 
-# %%
 
-# model_path = "llava-hf/llava-1.5-7b-hf"
-model_path = "llava-hf/llava-1.5-13b-hf"
-
-print(model_path)
-
-cache_dir = "/scratch/shiyesu/.cache/huggingface/hub"
-
-model = LlavaForConditionalGeneration.from_pretrained(model_path, cache_dir=cache_dir)
-model.eval()
-processor = AutoProcessor.from_pretrained(model_path, cache_dir=cache_dir)
-tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, cache_dir=cache_dir)
-
-# %%
-
-# device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
-model.to(device)
-
-# %%
 
 def prompt(text):
     return f"<image>\nUSER: {text}\nASSISTANT:"
+
 
 
 def parse_output(text):
     return text.split('ASSISTANT: ')[-1]
 
 
-# %%
-
-data = pd.read_csv("data/data.csv", index_col=0)
-
-for ind,dat in tqdm(data.iterrows()):
-
-    text = prompt(dat.text)
-    image = Image.open(f"data/pld/{dat.image}.png")
-
-    inputs = processor(text=text, images=image, return_tensors="pt").to(device)
-
-    generate_ids = model.generate(**inputs, max_length=128)
-    output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-
-    data.loc[ind,"output"] = parse_output(output)
-
-
-    data.to_csv(f"results/{model_path.split('/')[-1]}.csv")
-
-
-# %%
 
 def transparent_to_white(image):
     new_image = Image.new("RGBA", image.size, "WHITE")
@@ -64,5 +26,81 @@ def transparent_to_white(image):
     new_image.convert('RGB')
     return new_image
 
-transparent_to_white(image)
-# %%
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", help="Model path", type=str, 
+                        default="llava-hf/llava-1.5-7b-hf")
+    parser.add_argument("--cache_dir", help="Hugginface cache dir", type=str, 
+                        default="/scratch/shiyesu/.cache/huggingface/hub")
+    parser.add_argument("--data_index_fn", type=str, default="data/data.csv")
+    parser.add_argument("--image_dir", type=str, default="data/pld")
+    parser.add_argument("--out_dir", type=str, default="results")
+    parser.add_argument("--quantise", type=bool, help="Whether to run with bitsandbytes quantisation", default=True)
+    parser.add_argument("--verbose", type=bool, default=True)
+    args = parser.parse_args()
+
+    print(args)
+
+
+    assert args.model_path in ["llava-hf/llava-1.5-7b-hf", "llava-hf/llava-1.5-13b-hf"]
+
+
+    if args.quantise:
+        model = LlavaForConditionalGeneration.from_pretrained(
+            args.model_path,
+            cache_dir=args.cache_dir,
+            torch_dtype=torch.float16, 
+            low_cpu_mem_usage=True,
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+    else:
+        model = LlavaForConditionalGeneration.from_pretrained(
+            args.model_path,
+            cache_dir=args.cache_dir,
+        )
+        device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
+        # device = torch.device('cpu')
+        model.to(device)
+    
+    model.eval()
+    processor = AutoProcessor.from_pretrained(args.model_path, cache_dir=args.cache_dir)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, use_fast=False, cache_dir=args.cache_dir)
+
+
+    data = pd.read_csv(args.data_index_fn, index_col=0)
+
+
+    pbar = tqdm(
+        total=len(data),
+        desc=f"Querying...",
+        disable=not args.verbose,
+    )
+
+    for ind,dat in data.iterrows():
+
+        text = prompt(dat.text)
+        image = Image.open(f"{args.image_dir}/{dat.image}")
+
+        inputs = processor(text=text, images=image, return_tensors="pt")
+        if args.quantise: inputs = inputs.to('cuda')
+        else: inputs = inputs.to(device)
+
+        generate_ids = model.generate(**inputs, max_length=128)
+        output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+        data.loc[ind,"output"] = parse_output(output)
+
+
+        data.to_csv(f"{args.out_dir}/{args.model_path.split('/')[-1]}.csv")
+
+        pbar.update(1)
+
+
+
+if __name__ == "__main__":
+    main()
